6f839405ce6d688844d78dbc68442365afd7be0a
Thomasomoke <omokethomas644@gmail.com>
2025-11-27 23:13:11 +0300

    feat: implement booking repository with payment flow

--- lib/features/booking/data/booking_repository.dart
+++ lib/features/booking/data/booking_repository.dart
@@ -1,10 +1,19 @@
-
 import 'package:bus_pos/features/booking/data/booking_api.dart';
-
+import 'package:bus_pos/features/booking/models/bus.dart';
+import 'package:bus_pos/features/booking/models/enum.dart';
 
 class BookingRepository {
   final BookingApi _api = BookingApi();
 
-
-  
+  Future<Map<String, dynamic>> submitBooking({
+    required Map<String, dynamic> passengerData,
+    required BookingType bookingType,
+    required Bus bus,
+  }) async {
+    if (bookingType == BookingType.seat) {
+      return await _api.addPassengerBooking(passengerData);
+    } else {
+      return await _api.addParcelBooking(passengerData);
+    }
+  }
 }
